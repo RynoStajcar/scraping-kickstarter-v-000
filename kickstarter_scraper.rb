4fd84e25d91204a4abe62f56e3e3ae7fd622cfae
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-30 21:52:56 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,19 +8,18 @@ def create_project_hash
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
       # projects: kickstarter.css("li.project.grid_4")
-  title = project.css("h2.bbcard_name strong a").text
-        # title: project.css("h2.bbcard_name strong a").text
-
-    projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value
-        # image link: project.css("div.project-thumbnail a img").attribute("src").value
-      :discription => project.css("p.bbcard_blurb").text
-        # description: project.css("p.bbcard_blurb").text
-      :location => project.css("ul.project-meta span.location-name").text
-        # location: project.css("ul.project-meta span.location-name").text
-      :precent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-        # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-      }
-end
-projects
+    title = project.css("h2.bbcard_name strong a").text
+          # title: project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value
+          # image link: project.css("div.project-thumbnail a img").attribute("src").value
+        :discription => project.css("p.bbcard_blurb").text
+          # description: project.css("p.bbcard_blurb").text
+        :location => project.css("ul.project-meta span.location-name").text
+          # location: project.css("ul.project-meta span.location-name").text
+        :precent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+          # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+        }
+      end
+      projects
 end
